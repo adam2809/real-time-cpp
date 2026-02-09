d4b97e9cf87a0fdf6bb5474859fab78cef729882
Adam Kulesza <adam.kule@gmail.com>
2026-02-09 22:01:55 +0100

    werking outhal

--- examples/chapter02_02/led.cpp
+++ examples/chapter02_02/led.cpp
@@ -13,74 +13,34 @@
 
 #include <cstdint>
 
-class led
+enum port_direction
 {
-public:
-  // Use convenient class-specific typedefs.
-  using port_type = std::uint8_t;
-  using bval_type = std::uint8_t;
-
-  // The led class constructor.
-  explicit led(const port_type p, const bval_type b)
-    : port(p),
-      bval(b)
-  {
-    // Set the port pin value to low.
-    *reinterpret_cast<volatile bval_type*>(port) &= static_cast<bval_type>(~bval);
-
-    // Set the port pin direction to output.
-
-    // Note that the address of the port direction
-    // register is one less than the address
-    // of the port value register.
-    const auto pdir = static_cast<port_type>(port - 1U);
-
-    *reinterpret_cast<volatile bval_type*>(pdir) |= bval;
-  }
-
-  auto toggle() const -> void
-  {
-    // Toggle the LED via direct memory access.
-    *reinterpret_cast<volatile bval_type*>(port) ^= bval;
-  }
-
-private:
-  // Private member variables of the class.
-  const port_type port;
-  const bval_type bval;
+  eInput = 0U,
+  eOutput = 1U
 };
 
-namespace
+enum port_address
 {
-  // Create led_b5 on portb.5.
-  led led_b5
-  {
-    mcal::reg::portb,
-    mcal::reg::bval5
-  };
-  led led_red
-  {
-    mcal::reg::portd,
-    mcal::reg::bval3
-  };
-  led led_blue
-  {
-    mcal::reg::portd,
-    mcal::reg::bval5
-  };
+  ePortB = 0x25U,
+  ePortD = 0x2BU
+};
+
+static inline void gpio_set_dir(port_address port, uint8_t bit_index, port_direction dir)
+{
+  *((volatile uint8_t*) port - 1U) |= (dir << bit_index);
 }
 
-static inline void gpio_toggle(uint8_t port, uint8_t bit_index)
+static inline void gpio_toggle(port_address port, uint8_t bit_index)
 {
   *((volatile uint8_t*) port) ^= (1U << bit_index);
 }
 
-static inline void gpio_high(uint8_t port, uint8_t bit_index)
+static inline void gpio_high(port_address port, uint8_t bit_index)
 {
   *((volatile uint8_t*) port) |= (1U << bit_index);
 }
 
-static inline void gpio_low(uint8_t port, uint8_t bit_index)
+static inline void gpio_low(port_address port, uint8_t bit_index)
 {
   *((volatile uint8_t*) port) &= ~(1U << bit_index);
 }
@@ -89,21 +49,28 @@ auto main() -> int;
 
 auto main() -> int
 {
-  gpio_high(mcal::reg::portd, 5U);
+  gpio_set_dir(ePortD, 5U, eOutput);
+  gpio_set_dir(ePortD, 3U, eOutput);
+  gpio_set_dir(ePortB, 5U, eOutput);
+
+  gpio_high(ePortD, 5U);
   _delay_ms(1000);
 
-  gpio_high(mcal::reg::portd, 3U);
+  gpio_high(ePortD, 3U);
   _delay_ms(1000);
 
-  gpio_low(mcal::reg::portd, 3U);
+  gpio_low(ePortD, 3U);
   _delay_ms(1000);
 
-  gpio_low(mcal::reg::portd, 5U);
+  gpio_low(ePortD, 5U);
   _delay_ms(1000);
-  for (;;)
-  {
-    gpio_toggle(mcal::reg::portd, 5U);
-    gpio_toggle(mcal::reg::portd, 3U);
-    _delay_ms(1000);
-  }
+
+  gpio_toggle(ePortD, 5U);
+  gpio_toggle(ePortD, 3U);
+  gpio_toggle(ePortB, 5U);
+  _delay_ms(1000);
+
+  gpio_toggle(ePortD, 5U);
+  gpio_toggle(ePortD, 3U);
+  gpio_toggle(ePortB, 5U);
 }
