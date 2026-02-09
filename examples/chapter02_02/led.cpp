1687d3d7be2dd715bfbb650bc3ab95f5edb90ea6
Adam Kulesza <adam.kule@gmail.com>
2026-02-09 23:28:08 +0100

    ok onw final api plzzzzz

--- examples/chapter02_02/led.cpp
+++ examples/chapter02_02/led.cpp
@@ -14,35 +14,32 @@
 
 #include <cstdint>
 
-enum port_direction
+enum address_direction
 {
   eInput = 0U,
   eOutput = 1U
 };
 
-static inline void gpio_set_dir(volatile uint8_t* dir_port, uint8_t bit_index, port_direction dir)
+
+
+static inline void set_bit_at_address(volatile uint8_t* address, uint8_t bit_index)
 {
-  *(dir_port) |= (dir << bit_index);
+  *(address) |= (1U << bit_index);
 }
 
-static inline void gpio_toggle(volatile uint8_t* port, uint8_t bit_index)
+static inline void toggle_bit_at_address(volatile uint8_t* address, uint8_t bit_index)
 {
-  *(port) ^= (1U << bit_index);
+  *(address) ^= (1U << bit_index);
 }
 
-static inline void gpio_high(volatile uint8_t* port, uint8_t bit_index)
+static inline void clear_bit_at_address(volatile uint8_t* address, uint8_t bit_index)
 {
-  *(port) |= (1U << bit_index);
+  *(address) &= ~(1U << bit_index);
 }
 
-static inline void gpio_low(volatile uint8_t* port, uint8_t bit_index)
+static inline bool check_bit_at_address(volatile uint8_t* address, uint8_t bit_index)
 {
-  *(port) &= ~(1U << bit_index);
-}
-
-static inline bool gpio_read(volatile uint8_t* port, uint8_t bit_index)
-{
-  return (*(port) >> bit_index & 1U);
+  return (*(address) >> bit_index & 1U);
 }
 
 auto main() -> int;
@@ -50,16 +47,16 @@ auto main() -> int;
 auto main() -> int
 {
 
-  gpio_set_dir(&DDRD, 5U, eOutput);
-  gpio_set_dir(&DDRD, 3U, eOutput);
-  gpio_set_dir(&DDRB, 1U, eInput);
+  set_bit_at_address(&DDRD, 5U);
+  set_bit_at_address(&DDRD, 3U);
+  clear_bit_at_address(&DDRB, 1U);
   for (;;)
   {
-    gpio_high(&PORTD, 5U);
-    gpio_low(&PORTD, 3U);
+    set_bit_at_address(&PORTD, 5U);
+    clear_bit_at_address(&PORTD, 3U);
     _delay_ms(500U);
-    gpio_low(&PORTD, 5U);
-    gpio_high(&PORTD, 3U);
+    clear_bit_at_address(&PORTD, 5U);
+    set_bit_at_address(&PORTD, 3U);
     _delay_ms(500U);
   }
   
